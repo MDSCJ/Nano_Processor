c5ce8abb5a6a8261b2b1e2642c55ba0d22055659
Chamath Jayasinghe <m.d.s.chamath@gmail.com>
2026-05-08 23:30:08 +0530

    Update Program_ROM_gr_46.vhd

--- Nanoprocessor_Lite/sources_1/new/Program_ROM_gr_46.vhd
+++ Nanoprocessor_Lite/sources_1/new/Program_ROM_gr_46.vhd
@@ -33,15 +33,15 @@ end Program_ROM_gr_46;
 architecture Behavioral of Program_ROM_gr_46 is
     type rom_type is array (0 to 15) of std_logic_vector(7 downto 0);
     signal program : rom_type := (
-        -- Compute 1 + 2 + 3 = 6, display result on 7-segment via R3
-        -- RAM is pre-initialised: RAM[n] = n
-        "01010001",  -- 0: LOAD R1, RAM[1]   -> R1 = 1
-        "01100010",  -- 1: LOAD R2, RAM[2]   -> R2 = 2
-        "00000110",  -- 2: ADD  R1, R2        -> R1 = R1 + R2 = 3
-        "01110011",  -- 3: LOAD R3, RAM[3]   -> R3 = 3
-        "00001101",  -- 4: ADD  R3, R1        -> R3 = R3 + R1 = 6  (shown on display)
-        "11000101",  -- 5: JMP  5             -> halt (loop forever)
-        "00000000",  -- 6: NOP
+        -- Accumulate 1+2+3=6 step-by-step in R3 (shown on 7-seg display)
+        -- R3 starts at 0 after reset. RAM[n]=n pre-initialised.
+        "01010001",  -- 0: LOAD R1, RAM[1]  -> R1 = 1
+        "00001101",  -- 1: ADD  R3, R1      -> R3 = 0+1 = 1  (display: 1)
+        "01010010",  -- 2: LOAD R1, RAM[2]  -> R1 = 2
+        "00001101",  -- 3: ADD  R3, R1      -> R3 = 1+2 = 3  (display: 3)
+        "01010011",  -- 4: LOAD R1, RAM[3]  -> R1 = 3
+        "00001101",  -- 5: ADD  R3, R1      -> R3 = 3+3 = 6  (display: 6)
+        "11000110",  -- 6: JMP  6           -> halt (loop forever)
         "00000000",  -- 7: NOP
         "00000000",  -- 8: NOP
         "00000000",  -- 9: NOP
